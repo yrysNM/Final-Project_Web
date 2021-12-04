a5cf3a934ff0a66edcb1bba625f3f8fa69a45637
yrys_N.M <yrysbek.or.s@gmail.com>
2021-12-04 18:41:50 +0600

    change Header Text and finished footerMenu

---
+++ 9 pages/js/buildTables.js
@@ -0,0 +1,21 @@
+/* const createMens =  document.querySelector("[data-build = 'mensTableParent']"),
+					table =	document.createElement("table"),
+					thread = document.createElement("thead")
+					tr = document.createElement("tr");
+					th = [];
+
+th.length = 8;
+
+table.classList.add("table");
+
+for(let i = 0; i < th.length; i++ ) {
+	th[i] = document.createElement("th");
+	 th[i].setAttribute("scope", "col");
+	 th[i].innerHTML = "Hi";
+	tr.append(th[i]);
+}
+
+
+createMens.append(table);
+table.append(thread);
+thread.append(tr); */
