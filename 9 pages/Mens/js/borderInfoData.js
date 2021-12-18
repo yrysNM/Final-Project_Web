5c04a57595f03fe6b06285c2e5ba83d3a8e6e76a
yrys_N.M <yrysbek.or.s@gmail.com>
2021-12-18 17:54:30 +0600

    fixed js :)

--- 9 pages/Mens/js/borderInfoData.js
+++ 9 pages/Mens/js/borderInfoData.js
@@ -4,6 +4,7 @@ window.addEventListener("DOMContentLoaded",() => {
 
 	const listData = document.querySelector("ol"),
 				datas = listData.querySelectorAll("li"),
+				 boxs = document.querySelectorAll(".box1"),
 				tagA = document.querySelectorAll("#tagA");
 
 
--- 9 pages/Mens/js/changeBoxs.js
+++ 9 pages/Mens/js/changeBoxs.js
@@ -1,21 +1,32 @@
 window.addEventListener("DOMContentLoaded", () => {
 
 	const boxs = document.querySelectorAll(".box1"),
-			listChangeCoats = document.querySelector("[data-handle = 'coats']"),
-			listChangeJakets = document.querySelector("[data-handle = 'jakets']"),
+		//	listChangeCoats = document.querySelector("[data-handle = 'coats']"),
+		//	listChangeJakets = document.querySelector("[data-handle = 'jakets']"),
 			listBtnChanges = document.querySelectorAll("#li"),
 			tagA = document.querySelectorAll("#tagA"),
-			app = document.getElementById("app");
+			clearAllElemnts = document.querySelectorAll('[data-clear = "clearAll"]');
+		
 
 
-const metaData = {
-	arrBoxs: [],
-	objBoxs: {},
-	arrListData: [],
-	objListBtn: {},
-	changeobjBoxs: {},
-	changeobjBoxsHide:{}
-}
+	const metaData = {
+		arrBoxs: [],
+		objBoxs: {},
+		arrListData: [],
+		//objListBtn: {},
+		changeobjBoxs: {},
+		//changeobjHide:{},
+		changeobjShow: {}
+	}
+
+
+	const funcClearAll = () => {
+		clearAllElemnts.forEach(item => {
+			item.addEventListener("click", () => {
+				location.reload();
+			});
+		})
+	};
 
 
 
@@ -29,7 +40,8 @@ const metaData = {
 
 				listBtnChanges.forEach((item, j )=> {
 					 metaData.arrListData[j] = item.dataset.handle;
-					 metaData.objListBtn[j] = item;
+					// metaData.objListBtn[j] = item;
+					metaData.changeobjShow[j] = tagA[j]
 
 					item.addEventListener("click", () =>  {
 						
@@ -41,160 +53,96 @@ const metaData = {
 								if(metaData.arrBoxs[i].classList[1] == metaData.arrListData[j] && 
 														tagA[j].classList.contains("showStyle")) {
 										
-									//app.removeChild(metaData.objBoxs[i]);
 									metaData.objBoxs[i].style.display = "block";
 									metaData.changeobjBoxs[i] = metaData.objBoxs[i];
-									//app.append(metaData.changeobjBoxs[i]);
-									//	app.append(metaData.objBoxs[i]);
-
+										metaData.changeobjShow[j] = tagA[j]
+									//	metaData.changeobjHide[i] = getComputedStyle(metaData.objBoxs[i]).display;
 
 								}
-
-// 								else if(metaData.arrBoxs[i].classList[1] != metaData.arrListData[j] && 
-// 														tagA[j].classList.contains("showStyle")) {
-// 										
-// 									//app.removeChild(metaData.objBoxs[i]);
-// 									//metaData.objBoxs[i].style.display = "none ";
-// 									metaData.changeobjBoxs[i] = metaData.objBoxs[i];
-// 									//app.append(metaData.changeobjBoxs[i]);
-// 									//	app.append(metaData.objBoxs[i]);
-// 
-// 
-// 								}
 
 								else if(tagA[j].classList.contains("showStyle") && metaData.changeobjBoxs[i] != undefined)	{
 
-								 	if(metaData.changeobjBoxs[i].classList[1] ==  metaData.arrListData[j] && 
-								 		tagA[j].classList.contains("showStyle")) {
+								 	if(metaData.changeobjBoxs[i].classList[1] ==  metaData.arrListData[j] && tagA[j].classList.contains("showStyle")) {
 								 	
-											metaData.objBoxs[i].style.display = "block";
-								 	}
-								 
+											metaData.changeobjBoxs[i].style.display = "block";
+											metaData.changeobjShow[j] = tagA[j]
+										//	metaData.changeobjHide[i] = getComputedStyle(metaData.changeobjBoxs[i]).display;
+									} 
 
-									
+
 								}
 
+									
+								
+
 								else if(tagA[j].classList.contains("hideStyle") && metaData.changeobjBoxs[i] != undefined) {
+
 									if(metaData.changeobjBoxs[i].classList[1] == metaData.arrListData[j] && 
 										tagA[j].classList.contains("hideStyle")) {
 									
-										metaData.changeobjBoxs[i].style.display = "none";
-										//metaData.changeobjBoxsHide[i] = metaData.objBoxs[i];
-									
-									}
-									/* else if(metaData.changeobjBoxs[i].classList[1] != metaData.arrListData[j] && 
-										tagA[j].classList.contains("hideStyle")) {
-										metaData.changeobjBoxs[i].style.display = "block";
-									} */
-								}else {
-								 	metaData.objBoxs[i].style.display = "none";
+
+											metaData.changeobjBoxs[i].style.display = "none";
+											//metaData.changeobjHide[i] = getComputedStyle(metaData.changeobjBoxs[i]).display;
+										
+
+										} 
+										else if(metaData.changeobjBoxs[i].classList[1] != metaData.arrListData[j])
+										{
+											//metaData.changeobjBoxs[i].style.display = "block";
+											for(let k = 0;k < tagA.length; k++)	{
+													if(tagA[k].classList.contains("showStyle")) {
+														if(metaData.changeobjBoxs[i].classList[1] == metaData.arrListData[k]) {
+														//	metaData.changeobjBoxs[i].style.display = "block";
+																
+
+														}
+													
+													} 
+											}
+										}
+
+										if(metaData.changeobjShow[0].classList.contains("hideStyle") && metaData.changeobjShow[1].classList.contains("hideStyle") && 
+															metaData.changeobjShow[2].classList.contains("hideStyle")) {
+																/* for(let i = 0;  i < boxs.length; i++) {
+																
+																	 metaData.objBoxs[i].style.display = "block";
+																	 break;
+																		
+																} */
+											location.reload();
+
+										}
+
+
+								}
+
+									else {
+
+								 			 metaData.objBoxs[i].style.display = "none";	
+
 								}	
+												
+													
 						})
 
 					});
-				})
 
+													
+													
+							
+				})
 
 			console.log(metaData);
+
+		
+			
 	};
+
+
+
 
 changeBoxs();
+funcClearAll();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*------------------------------------------------------------------------------------------------------------------------------------ */
-
-	const changeCoats = () => {
-
-			listChangeCoats.addEventListener("click", () => {
-
-				boxs.forEach((item, i) => {
-					if(!item.classList.contains("coats") &&  tagA[0].classList.contains("showStyle")) {
-						
-						//item.classList.remove("showBoxs");
-						//item.classList.add("hideBoxs");
-						item.style.display = "none";
-					}
-
-					if(tagA[0].classList.contains("hideStyle"))
-					{
-						item.style.display = "block";
-						// item.classList.remove("hideBoxs");
-						// item.classList.add("showBoxs");;
-					}
-
-				})
-
-			});
-	};
-
-
-	
-
-	const changeBoxsJackests = () => {
-
-		listChangeJakets.addEventListener("click", () =>{
-			boxs.forEach((item, i) => {
-				
-
-				 if(!item.classList.contains("jakets") && tagA[1].classList.contains("showStyle")) {
-					// item.classList.remove("showBoxs");
-					// item.classList.add("hideBoxs");
-					item.style.display = "none";
-				}
-
-				
-
-				if(tagA[1].classList.contains("hideStyle")) {
-					item.style.display = "block";
-					// item.classList.remove("hideBoxs");
-					// item.classList.add("showBoxs");
-				}
-
-			})
-		});
-	};
-
-	const changeBoxsJackestsCoats = () => {
-
-		listChangeJakets.addEventListener("click", () =>{
-			boxs.forEach((item, i) => {
-				
-
-				 if(!item.classList.contains("jakets") && !item.classList.contains("coats")  && 
-				 		tagA[1].classList.contains("showStyle") &&  tagA[0].classList.contains("showStyle")) {
-
-					// item.classList.remove("showBoxs");
-					// item.classList.add("hideBoxs");
-					item.style.display = "none";
-				}
-
-				
-
-				if(tagA[1].classList.contains("hideStyle") && tagA[0].classList.contains("hideStyle")) {
-					item.style.display = "block";
-				}
-
-			})
-		});
-	};
-// 
-//  	changeBoxsJackests();
-//  
-//  	changeCoats();
-//  
-//  	changeBoxsJackestsCoats();
 
 });
